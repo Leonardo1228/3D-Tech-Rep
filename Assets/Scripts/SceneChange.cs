de520b9a2ce5e78f6523d79d262b87306ba63d80
Leonardo1228 <142549969+Leonardo1228@users.noreply.github.com>
2026-04-27 18:59:35 -0500

    delete old

--- Assets/Scripts/SceneChange.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class SceneChange : MonoBehaviour
-{
-    public void SceneCharge(int Nescene){
-        SceneManager.LoadScene(Nescene);
-     }
-}
