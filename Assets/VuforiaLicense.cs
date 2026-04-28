f8c8e1acf04856825d8065244a5a27e478694c75
Leonardo1228 <142549969+Leonardo1228@users.noreply.github.com>
2026-04-27 21:11:43 -0500

    add

---
+++ Assets/VuforiaLicense.cs
@@ -0,0 +1,14 @@
+
+            //This class is auto-generated, please do not overwrite
+            using UnityEngine.Scripting;
+
+            public class VuforiaLicense
+            {
+const string license = "QVMwOGRQTC8vLy8vQUFBQm1ZR0ExMWpHNFV2N29yVEFPTDZJWlYxMkZnU3lCV202SEEyemJKL3lRYzRQV2Q2YW1kcjRiOTRaNlAyUklnU2JrRExBWU1CdXlsT0YxKzNqK3pRYnlURms1dkU1RDNYZVFvMThJTnJFWXVrSGZOV1BZSWN4ODdzTzhYcUZVRFpBMHc2RTRmeFlJOXl4WGtYZ1Byclkxc1hKV0RjS3VQcjFXaUcvSCt4Y2E2NGFwZHYwVGZTYmVGQWtvUVhYVFpCNGF5bGVUQW9qdHZNcW5qMURqMlZvZHNKOWk4YXUwcmxHdHNCWU5tcWpWR0g1NGc0L0RNVGNpbVBRM3BJcG5sYThBZXBtRmw1dWg4ejFyeGVoQWVaU01DcVM4UFVSeEpPZ3B3azBtYkJqVGNEWE9PRlFjWXFvckV1bnh2TEd0SDRBSXBNQ1VSVjlwYVBxU2djVlpyajlyZjJqNURKTUFxVVVMTDNGRTNzWFYybGk=";
+
+                [Preserve]
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
